526e1cb2509cc5dbac2b340a4939c0cdc9e35741
jinn-ie <lsj020724@naver.com>
2025-02-06 11:12:36 +0900

    add gitignore

--- Day3/database.py
+++ Day3/database.py
@@ -6,6 +6,12 @@ from sqlmodel import (
     create_engine, select
 )
 
+from post_service import *
+
+# 환경변수 설정
+from dotenv import load_dotenv
+import os
+
 #from app.services.post_service import *
 
 # id, created_at은 직접 입력하기 어려움
@@ -16,22 +22,19 @@ class PostReq:
     body: str
     published: bool
 
-# db 테이블 정의
-class Post(SQLModel, table=True):
-    id: int | None = Field(primary_key=True)
-    created_at: int = Field(index=True)
-    published: bool = Field(index=True)
-    title: str
-    body: str
-
 # 명확한 형식, 에러 메시지 확장 가능
 @dataclass
 class PostsResp:
     posts: list[Post]
     err: str | None = None
 
+
+# .env 파일 로드
+load_dotenv()
+
 # db 연결 설정
-db_url = "sqlite:///Day3/blog.db" # 파일명
+db_url = os.getenv("DB_HOST")
+db_pwd = os.getenv("DB_PWD")
 db_engine = create_engine(db_url, connect_args={"check_same_thread": False}) # 다중 스레드 접근
 
 # db 세션 설정 (커밋, 롤백 객체)
@@ -51,77 +54,58 @@ create_db()
 
 # 게시글 생성
 @app.post("/posts")
-def create_post(post: PostReq,
-               db = Depends(get_db_session)):
+def create_post(req: PostReq,
+                db = Depends(get_db_session),
+                postService: PostService = Depends()):
     
-# def create_post(post: PostReq,
-#                 db = Depends(get_db_session),
-#                 postService = Depends()):
-    
-    # resp = postService.create_post(db, post)
-    
-    postModel = Post()
-    postModel.title= post.title
-    postModel.body = post.body
-    postModel.published = post.published
-    postModel.created_at = int(time.time())
-    
-    db.add(postModel)
-    db.commit()
-    db.refresh(postModel) # db에 저장된 데이터를 다시 가져옴 (id, created_at)
-    return postModel
+    post = postService.create_post(db, req)
+   
+    resp = PostsResp(posts=[post])
+    return resp
 
 # 게시글 목록 조회
 @app.get("/posts")
 def get_posts(page: int=1, limit: int=2,
-              db = Depends(get_db_session)):
+              db = Depends(get_db_session),
+              postService: PostService = Depends()) -> PostsResp:
     if page < 1: page = 1
     if limit < 1: return []
     if limit > 2: limit = 2
 
-    nOffset = (page - 1) * limit
-    # select로 쿼리 가져옴 -> 실행 및 결과 리스트 반환
-    posts = db.exec(select(Post).offset(nOffset).limit(limit)).all()
+    posts = postService.get_posts(db, page, limit)
 
     # 관리하기 더 편함
     resp = PostsResp(posts=posts)
     return resp
 
-
 # 게시글 조회
 @app.get("/posts/{post_id}")
 def get_post(post_id: int,
-             db = Depends(get_db_session)):
-    post = db.get(Post, post_id)
+             db = Depends(get_db_session),
+             postService: PostService = Depends()) -> PostsResp:
+    
+    # 함수 호출
+    post = postService.get_post(db, post_id)
+
     if not post:
-        raise HTTPException(status_code=404, detail="Post not found")
+        raise HTTPException(status_code=404,
+                            detail="Post not found")
     
     resp = PostsResp(posts=[post]) # 데이터 형식 유지
     return resp
 
-# 게시글 삭제
-@app.delete("/posts/{post_id}")
-def delete_post(post_id: int,
-                db=Depends(get_db_session)):
-    post = db.get(Post, post_id)
-    if not post:
-        raise HTTPException(status_code=404, detail="Post not found")
-    db.delete(post)
-    db.commit()
-    return {
-        'ok': True
-    }
-
 # 게시글 수정
 @app.put("/posts/{post_id}")
-def update_post(post_id: int, reqBody: PostReq,
-                db=Depends(get_db_session)):
+def update_post(post_id: int, req: PostReq,
+                db=Depends(get_db_session),
+                postService: PostService = Depends()):
     post = db.get(Post, post_id)
+    
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
     
     # dataclass -> dict
-    dictToUpdate = asdict(reqBody)
+    dictToUpdate = asdict(req)
     post.sqlmodel_update(dictToUpdate)
     db.add(post)
     db.commit()
@@ -129,6 +113,17 @@ def update_post(post_id: int, reqBody: PostReq,
 
     resp = PostsResp(posts=[post])
     return resp
+
+# 게시글 삭제
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int,
+                db=Depends(get_db_session),
+                postService: PostService = Depends()):
+    if not postService.delete_post(db, post_id):
+        raise HTTPException(status_code=404, detail="Post not found")
+    return {
+        'ok': True
+    }
 
 # ctrl + shift + p -> sqlite3
 # sqlite> open database
--- Day3/post_service.py
+++ Day3/post_service.py
@@ -1,6 +1,10 @@
 
 from dataclasses import dataclass
-from sqlmodel import Session
+from sqlmodel import (
+    Field, SQLModel,
+    Session, select
+)
+import time
 
 @dataclass
 class PostReq:
@@ -8,19 +12,52 @@ class PostReq:
     body: str
     published: bool
 
+# db 테이블 정의
+class Post(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    created_at: int = Field(index=True)
+    published: bool = Field(index=True)
+    title: str
+    body: str
+
 class PostService:
+    def create_post(self, db: Session, req: PostReq):
+                
+        post = Post()
+        post.title= req.title
+        post.body = req.body
+        post.published = req.published
+        post.created_at = int(time.time())
+        
+        db.add(post)
+        db.commit()
+        db.refresh(post) # db에 저장된 데이터를 다시 가져옴 (id, created_at)
+        
+        return post
+
+    def get_posts(self, db: Session, page: int=1, limit: int=10):
+        nOffset = (page - 1) * limit
+
+        # select로 쿼리 가져옴 -> 실행 및 결과 리스트 반환
+        posts = db.exec(
+            select(Post).offset(nOffset).limit(limit)
+            ).all()
+
+        return posts
+
     def get_post(self, db: Session, post_id: int):
-        pass
-
-    def get_posts(self, db: Session, page: int=1):
-        pass
-
-    def create_post(self, db: Session, post: PostReq):
-        pass
+        
+        post = db.get(Post, post_id)
+        return post
 
     def update_post(self, db: Session,
-                    post_id: int, post: PostReq):
+                    post_id: int, req: PostReq):
         pass
 
-    def delete_post(self, db: Session, post_id: int):
-        pass
+    def delete_post(self, db: Session, post_id: int) -> bool:
+        post = db.get(Post, post_id)
+        if not post:
+            return False
+        db.delete(post)
+        db.commit()
+        return True
